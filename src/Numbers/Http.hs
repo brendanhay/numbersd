3fddcf25502741b6940d71efe83cba19001c3e48
Brendan Hay <brendan.g.hay@gmail.com>
2012-12-11 16:22:01 +0100

    Adding preliminary support for format= query parameter

--- src/Numbers/Http.hs
+++ src/Numbers/Http.hs
@@ -15,7 +15,6 @@ module Numbers.Http (
     ) where
 
 import Blaze.ByteString.Builder hiding (flush)
-import Control.Monad
 import Control.Monad.IO.Class
 import Control.Concurrent.Async
 import Data.Maybe
@@ -27,6 +26,8 @@ import Numbers.Types
 import Numbers.Conduit
 
 import qualified Numbers.Whisper as W
+
+data Format = Raw | Pretty
 
 sinkHttp :: Int -> Int -> Maybe Int -> Maybe (IO EventSink)
 sinkHttp res step = fmap $ \port -> do
@@ -40,22 +41,36 @@ sinkHttp res step = fmap $ \port -> do
 -- | serves whispers as if served by graphite http://graphite.wikidot.com/url-api-reference
 serve :: W.Whisper -> Request -> IO Response
 serve whis req = case pathInfo req of
-    ["numbersd"]           -> series whis (getTargets req)
-    ["numbersd", "render"] -> series whis (getTargets req)
-    _                      -> return unknown
+    ("numbersd":_) -> series whis (getFormat req) (getTargets req)
+    _              -> return unknown
 
-series :: W.Whisper -> Maybe [Key] -> IO Response
-series whis mks = do
-    ts <- currentTime
-    response status200 `liftM` W.raw ts ts whis mks
+series :: W.Whisper -> Format -> Maybe [Key] -> IO Response
+series whis fmt mks = do
+    now  <- currentTime
+    body <- case fmt of
+        Pretty -> W.raw now now whis mks
+        Raw    -> W.raw now now whis mks
+    return $ response status200 body
+
+getFormat :: Request -> Format
+getFormat req = case maybeParams fmt req of
+    Just (f:_) -> f
+    _          -> Pretty
+  where
+    fmt ("format", Just "raw") = Just Raw
+    fmt _                      = Nothing
 
 getTargets :: Request -> Maybe [Key]
-getTargets =
-    (\xs -> if null xs then Nothing else Just xs) . catMaybes . map target . queryString
+getTargets = maybeParams target
   where
-    target :: QueryItem -> Maybe Key
     target ("target", Just x) = Just $ Key x
-    target _ = Nothing
+    target _                  = Nothing
+
+maybeParams :: (QueryItem -> Maybe a) -> Request -> Maybe [a]
+maybeParams f = g . catMaybes . map f . queryString
+  where
+    g [] = Nothing
+    g xs = Just xs
 
 unknown :: Response
 unknown = response status404 $ copyByteString "Error: Not Found"
