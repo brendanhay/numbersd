d9277f5f0437851f6d45fa7afa45cebd205038ab
Brendan Hay <brendan.g.hay@gmail.com>
2012-12-15 11:13:58 +0100

    Lowering the size from 12 -> 8

--- src/Numbers/Config.hs
+++ src/Numbers/Config.hs
@@ -91,7 +91,7 @@ defaultConfig :: Config
 defaultConfig = Config
     { _listeners    = [Udp (BS.pack "0.0.0.0") 8125]
     , _httpPort     = Nothing
-    , _buffer       = 12
+    , _buffer       = 8
     , _interval     = 10
     , _resolution   = 60
     , _percentiles  = [90]
